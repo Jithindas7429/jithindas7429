4baedc68664f7a0ea1103b376cbf7dbdb44e1888
Jithindas7429 <Jithindas7429@gmail.com>
2026-01-19 19:54:47 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+// PushAlert-ൽ നിന്നുള്ള കോഡ് ഇവിടെ ചേർക്കുക
+importScripts("https://cdn.pushalert.co/sw-87210.js");
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  // PWA ഇൻസ്റ്റാൾ ആകാൻ ഇത് നിർബന്ധമാണ്
+});
