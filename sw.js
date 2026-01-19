98162741cecbf1cd246e822f33682fc26f27cbe7
Jithindas7429 <Jithindas7429@gmail.com>
2026-01-19 19:56:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // PushAlert-ൽ നിന്നുള്ള കോഡ് ഇവിടെ ചേർക്കുക
-importScripts("https://cdn.pushalert.co/sw-87210.js");
+importScripts("https://cdn.pushalert.co/sw-87210_4.js");
 
 self.addEventListener('install', e => {
   self.skipWaiting();
@@ -11,4 +11,5 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   // PWA ഇൻസ്റ്റാൾ ആകാൻ ഇത് നിർബന്ധമാണ്
+
 });
